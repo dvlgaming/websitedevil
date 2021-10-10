a322799835f8b41d1e4819927f9371a56274ae16
dvlgaming <rajifhabibi56@gmail.com>
2021-10-10 15:05:24 +0700

    web devil

---
+++ js/click.js
@@ -0,0 +1,5 @@
+function openLink() {
+
+    window.open('https://api.whatsapp.com/send?phone=6288215463787&text=bang mau sewa bot', '_blank')
+
+}
---
+++ routes/main.js
@@ -0,0 +1,36 @@
+__path = process.cwd()
+
+var express = require('express');
+var router = express.Router();
+
+router.get('/', (req, res) => {
+    res.sendFile(__path + './home.html')
+})
+
+router.get('/api', (req, res) => {
+    res.sendFile(__path + './index.html')
+})
+
+router.get('/api/about', (req, res) => {
+    res.sendFile(__path + './about.html')
+})
+
+router.get('/api/news', (req, res) => {
+    res.redirect('https://raw.githubusercontent.com/Zhirrr/My-SQL-Results/main/Berita.json')
+})
+
+router.get('/config', (req, res) => {
+    config = {
+        status: true,
+        result: {
+            prefix : '#',
+            namabot: 'ZhirrrBot',
+            namaowner: 'Zhirrr',
+            instagram: 'zhirr_ajalah',
+            youtube : 'Gak Punya'
+        }
+    }
+    res.json(config)
+})
+
+module.exports = router
